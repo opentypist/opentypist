dbf8f73c4b26ba25564a97c26250669d1c0f262c
wpdough <me@wpdough.com>
2021-08-06 21:54:12 -0700

    add basic highlighting and wpm calculation

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -1,8 +1,44 @@
-$( document ).ready(function() {
+let challengeText = "If you feel like there's something out there that you're supposed to be doing, if you have a passion for it, then stop wishing and just do it.";
+let challengeWords = challengeText.split(" ");
+
+function highlightWordUpTo(index) {
+    let text = "";
+    for (let i = 0; i < challengeWords.length; i++) {
+        let word = challengeWords[i];
+        if (i < index)
+            word = `<span class="color-correct">` + word + `</span>`;
+        text = text + word + " ";
+    }
+    $('#challenge').html(text);
+}
+
+function updateWordsPerMinute(startTime, wordsTyped) {
+    let millis = new Date().getTime() - startTime;
+    let wpm = Math.floor(wordsTyped / (millis / (60 * 1000)));
+    $('#stats').text(wpm + " wpm");
+}
+
+$(document).ready(() => {
+    // setup challenge text
+    $('#challenge').text(challengeText);
+
+    let wordIndex = 0;
+    let startTime = -1;
+
     let typer = $('#typer');
     typer.on('input', () => {
+        if (startTime == -1)
+            startTime = new Date().getTime();
+
+        updateWordsPerMinute(startTime, wordIndex);
+
         if (typer.val().endsWith(" ")) {
-            typer.val("");
+            let currentWord = challengeWords[wordIndex];
+            if (typer.val().substring(0, typer.val().length - 1) == currentWord) {
+                wordIndex++;
+                typer.val("");
+                highlightWordUpTo(wordIndex);
+            }
         }
     });
 });
