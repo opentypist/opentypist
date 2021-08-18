ad5dd1d3e900f05d98be4537e7fd5f350a2512dc
wpdough <me@wpdough.com>
2021-08-17 22:36:10 -0700

    add endpoint to post typing results

--- src/main/java/com/opentypist/controller/ResultController.java
+++ src/main/java/com/opentypist/controller/ResultController.java
@@ -5,14 +5,14 @@ import com.opentypist.persist.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
 public class ResultController {
     @Autowired
     private ResultRepository resultRepository;
@@ -35,5 +35,13 @@ public class ResultController {
         }
 
         return modelAndView;
+    }
+
+    @PostMapping("/post-result")
+    public Map<String, String> postResult(@RequestParam Integer speed) {
+        Result result = resultRepository.save(new Result(speed));
+        Map<String, String> map = new HashMap<>();
+        map.put("id", result.getId().toString());
+        return map;
     }
 }
