e9bf87018ea83b6a561d37dd76fda8e3d72da7fd
James Moles <jamespmoles@gmail.com>
2021-08-29 23:14:09 -0400

    added individual quote high scores pages

--- src/main/java/com/opentypist/controller/HighscoresController.java
+++ src/main/java/com/opentypist/controller/HighscoresController.java
@@ -1,17 +1,24 @@
 package com.opentypist.controller;
 
+import com.opentypist.persist.Quote;
+import com.opentypist.persist.QuoteRepository;
 import com.opentypist.persist.Result;
 import com.opentypist.persist.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
 public class HighscoresController {
+
+    @Autowired
+    private QuoteRepository quoteRepository;
 
     @Autowired
     private ResultRepository resultRepository;
@@ -27,4 +34,27 @@ public class HighscoresController {
         mav.addObject("results", results);
         return mav;
     }
+
+    @GetMapping("/highscores-quote")
+    public ModelAndView highscoresQuote(@RequestParam Integer quoteId) {
+        ModelAndView mav = new ModelAndView("highscores-quote");
+
+        List<Result> results = resultRepository.findAll().stream()
+                .filter(result -> result.getQuote() != null && result.getQuote().getId().equals(quoteId))
+                .sorted((r1, r2) -> Integer.compare(r2.getSpeed(), r1.getSpeed()))
+                .collect(Collectors.toList());
+
+        Optional<Quote> quote = quoteRepository.findById(quoteId);
+        String quoteText;
+
+        if(quote.isPresent())
+            quoteText = quote.get().getQuote();
+        else
+            quoteText = "";
+
+        mav.addObject("results", results);
+        mav.addObject("quoteText", quoteText);
+        return mav;
+    }
+
 }
